0d2a5215567a83925b9f05c66acc6a39671fcf8c
Graeme Coupar <grambo@grambo.me.uk>
2013-11-21 23:56:49 +0000

    Timestamps now get converted to ISO times

--- main.py
+++ main.py
@@ -10,17 +10,23 @@ app.config.DEBUG = True
 {"@source":"unknown","@type":null,"@tags":[],"@fields":{},"@message":"Hello world","@timestamp":"2012-06-26T15:58:20.135353Z"}
 '''
 
+def _timestamp_to_iso(ts):
+    return datetime.fromtimestamp(log['start_time']).isoformat()
+
+
 def _to_logstash_format(log):
     '''
     Converts an appengine log dict to logstash format
     '''
+    log['start_time'] = _timestamp_to_iso(log['start_time'])
+    log['end_time'] = _timestamp_to_iso(log['end_time'])
     return {'@source': 'appengine://' + log['app_id'],
             '@type': 'appengine',
             '@tags': [],
             '@fields': {k: v for k, v in log.iteritems()
                         if v and k not in ['message', 'app_id']},
             '@message': log['message'],
-            '@timestamp': datetime.fromtimestamp(log['start_time']).isoformat()}
+            '@timestamp': log['start_time']}
 
 
 @app.route("/logs", methods=['PUT'])
